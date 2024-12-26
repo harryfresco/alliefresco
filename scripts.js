06429d0f7347e122fb97292399a9c3c15f8fc019
Harry Fresco <harryfresco99@gmail.com>
2024-12-26 19:21:08 +0000

    added more stuff

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
 function showVideo(video){
     let div = document.getElementById('popup');
+    if (video == 'foryou'){
+        div.innerHTML="<iframe width='420' height='345' src=' https://www.youtube.com/embed/QNJ6KpKofUo?si=t4JxK2Xc7_huCyBp' class='centreClass' allow='fullscreen;' anonymous> </iframe>"
+        div.style.display="block"
+       
+    }
+    else{
     div.innerHTML=" <iframe width='420' height='345' src='https://www.youtube.com/embed/tgbNymZ7vqY' class='centreClass' allow='fullscreen;'> </iframe>"
     div.style.display="block"
+    }
 }
 
 function hidePopup(){
